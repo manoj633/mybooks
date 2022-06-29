27b15115ccf4de719bef5f117e2a62cf55f13295
Manoj <manoj09633@gmail.com>
2022-06-29 20:20:45 +0530

    Initial commit

---
+++ src/test/java/com/mybooks/MybooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
