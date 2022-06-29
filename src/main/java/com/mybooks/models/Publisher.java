3a14afa07865bef581e819f6da226390cc857d56
Manoj <manoj09633@gmail.com>
2022-06-29 20:47:35 +0530

    Added models and repository

---
+++ src/main/java/com/mybooks/models/Publisher.java
@@ -0,0 +1,125 @@
+package com.mybooks.models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String addressline;
+    private String city;
+    private String state;
+    private String zip;
+
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
+
+    public Publisher() {
+    }
+
+    public Publisher(String name, String addressline, String city, String state, String zip) {
+        this.name = name;
+        this.addressline = addressline;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public Publisher(String name, String addressline, String city, String state, String zip, Set<Book> books) {
+        this.name = name;
+        this.addressline = addressline;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressline() {
+        return addressline;
+    }
+
+    public void setAddressline(String addressline) {
+        this.addressline = addressline;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", addressline1='" + addressline + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return id != null ? id.equals(publisher.id) : publisher.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+}
