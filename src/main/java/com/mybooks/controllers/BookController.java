cfc259d76f2bb61acf76aca8a50b42854f7367f7
Manoj <manoj09633@gmail.com>
2022-06-30 20:59:25 +0530

    Thymeleaf Pagination successful

--- src/main/java/com/mybooks/controllers/BookController.java
+++ src/main/java/com/mybooks/controllers/BookController.java
@@ -1,25 +1,56 @@
 package com.mybooks.controllers;
 
+import com.mybooks.models.Book;
 import com.mybooks.repositories.BookRepository;
+import com.mybooks.services.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class BookController {
 
-    @Autowired
-    private final BookRepository bookRepository;
+    private final BookServiceImpl bookService;
 
-    public BookController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    public BookController(BookServiceImpl bookService) {
+        this.bookService = bookService;
     }
 
     @RequestMapping("/books")
     public String getBooks(Model model) {
-        model.addAttribute("books", bookRepository.findAll());
+        model.addAttribute("books", bookService.getAllBooks());
         return "books/list";
+    }
+
+    @RequestMapping(value = "/listBooks", method = RequestMethod.GET)
+    public String listBooks(Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size){
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(5);
+
+        Page<Book> bookPage = bookService.findPaginated(PageRequest.of(currentPage-1, pageSize));
+        model.addAttribute("bookPage",bookPage);
+        model.addAttribute("books", bookService.getAllBooks());
+        List<HashMap<String, String>> msg = new ArrayList<>();
+        msg.add((HashMap<String, String>) new HashMap<>().put("id", "Book id"));
+        msg.add((HashMap<String, String>) new HashMap<>().put("name", "Book Name"));
+        model.addAttribute("msg", msg);
+
+
+        int totalPages = bookPage.getTotalPages();
+        if(totalPages>0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+        return "books/listBooks";
     }
 
 }
--- src/main/java/com/mybooks/services/BookServiceImpl.java
+++ src/main/java/com/mybooks/services/BookServiceImpl.java
@@ -4,16 +4,24 @@ import com.mybooks.exceptions.BookNotFoundException;
 import com.mybooks.models.Book;
 import com.mybooks.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class BookServiceImpl implements BookService {
 
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @Override
     public Book saveBook(Book book) {
@@ -45,4 +53,24 @@ public class BookServiceImpl implements BookService {
     public void updateBook(Book book) {
         bookRepository.save(book);
     }
+
+    public Page<Book> findPaginated(Pageable pageable) {
+        int pageSize = pageable.getPageSize();
+        int currentPage = pageable.getPageNumber();
+        int startItem = currentPage * pageSize;
+        List<Book> list;
+
+        if (((List<Book>)bookRepository.findAll()).size() < startItem) {
+            list = Collections.emptyList();
+        } else {
+            int toIndex = Math.min(startItem + pageSize, ((List<Book>)bookRepository.findAll()).size());
+            list = ((List<Book>)bookRepository.findAll()).subList(startItem, toIndex);
+        }
+
+        Page<Book> bookPage
+                = new PageImpl<Book>(list, PageRequest.of(currentPage, pageSize), ((List<Book>)bookRepository.findAll()).size());
+
+        return bookPage;
+    }
+
 }
